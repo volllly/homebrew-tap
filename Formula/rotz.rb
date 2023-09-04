b7f511e5b2137bde9a1ade0c7866352755bcc3e9
Paul Volavsek <paul.volavsek@gmail.com>
2023-09-04 19:55:21 +0200

    fix lint

--- Formula/rotz.rb
+++ Formula/rotz.rb
@@ -1,8 +1,7 @@
 class Rotz < Formula
-  desc "Fully cross platform dotfile manager and dev environment bootstrapper written in Rust. "
-  homepage " volllly.github.io/rotz/"
-  version "0.9.5"
-  url "https://github.com/volllly/rotz/archive/refs/tags/v#{version}.tar.gz"
+  desc "Fully cross platform dotfile manager and dev environment bootstrapper"
+  homepage "https://volllly.github.io/rotz/"
+  url "https://github.com/volllly/rotz/archive/refs/tags/v0.9.5.tar.gz"
   sha256 "6b6742ffbcd2013c394fa3f50b6890eb84c5b7e9e2854daecfdc3cbd4106f3dc"
   license "MIT"
   head "https://github.com/volllly/rotz.git", branch: "main"
@@ -17,8 +16,8 @@ class Rotz < Formula
     assert_match "rotz #{version}\n", shell_output("#{bin}/rotz -V")
 
     err = capture(:stderr) { system bin/"rotz", "link" }
-    
-    assert_match /Error\: dotfiles\:\:walk/, err
-    assert_match /The system cannot find the file specified/, err
+
+    assert_match(/Error: dotfiles::walk/, err)
+    assert_match(/The system cannot find the file specified/, err)
   end
 end
